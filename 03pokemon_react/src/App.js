c9f780e6d21d86165543d52955f8b41a9e2c9ba0
Chimoha31 <mokochii1108@gmail.com>
2022-12-14 14:42:18 -0800

    Get pokemon data

--- 03pokemon_react/src/App.js
+++ 03pokemon_react/src/App.js
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./components/Card";
+import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
 
 const App = () => {
-  return <div className="App">App</div>;
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // All Polemon
+      let res = await getAllPokemon(initialURL);
+      // Each Pokemon
+      loadPokemon(res.results);
+      setLoading(false);
+    };
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  console.log(pokemonData);
+
+  return (
+    <div className="App">
+      {loading ? (
+        <div>loading</div>
+      ) : (
+        <>
+          <div className="pokemonCardContainer">
+            {pokemonData.map((pokemon, index) => (
+             <Card key={index} pokemon={pokemon} />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default App;
