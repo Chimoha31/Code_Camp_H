01c92300f3e2fb262c4fea68c44e40d5c075615e
Chimoha31 <mokochii1108@gmail.com>
2022-12-14 14:59:34 -0800

    Display Pokemon

--- 03pokemon_react/src/components/Card.jsx
+++ 03pokemon_react/src/components/Card.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./Card.css";
 
-const Card = ({pokemon}) => {
+const Card = ({ pokemon }) => {
   return (
-    <div>
-      Card
+    <div className="card">
+      <div className="cardImg">
+        <img src={pokemon.sprites.front_default} alt="pokemon" />
+      </div>
+      <h3 className="cardName">{pokemon.name}</h3>
+      <div className="cardTypes">
+        <div>Type</div>
+        {pokemon.types.map((type, index) => (
+          <div key={index}>
+            <span className="typeName">{type.type.name}</span>
+          </div>
+        ))}
+      </div>
+      <div className="cardInfo">
+        <div className="cardData">
+          <p className="title">Weight: {pokemon.weight}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Height: {pokemon.height}</p>
+        </div>
+        <div className="cardData">
+          <p className="title">Ability: {pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
