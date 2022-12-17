ed4f1c1c9a8168d811a77f76adc92fd1200d7887
Chimoha31 <mokochii1108@gmail.com>
2022-12-17 14:53:56 -0800

    Implementing Login

--- 04blog_firebase/src/components/login/Login.jsx
+++ 04blog_firebase/src/components/login/Login.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
+import { signInWithPopup } from "firebase/auth";
+import { provider, auth } from "../../firebase/firebase";
 
 const Login = () => {
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        // const user = result.user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-      Login
+      <p>Get started !</p>
+      <button onClick={handleGoogleLogin}>Login with Google</button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
