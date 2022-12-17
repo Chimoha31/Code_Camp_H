96230e47cb986df55d3573c60422adbaa099d7e1
Chimoha31 <mokochii1108@gmail.com>
2022-12-16 22:08:39 -0800

    Installed firebase

---
+++ 04blog_firebase/src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
