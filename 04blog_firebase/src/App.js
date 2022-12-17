e1874871e1c82d1c0540db0ba40900c1de90f6bd
Chimoha31 <mokochii1108@gmail.com>
2022-12-16 22:24:01 -0800

    Installed reactrouterdom

--- 04blog_firebase/src/App.js
+++ 04blog_firebase/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Logout from "./components/logout/Logout";
+import CreatePost from "./components/post/CreatePost";
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/createPost" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- 04blog_firebase/src/firebase/firebase.js
+++ 04blog_firebase/src/firebase/firebase.js
@@ -3,7 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-
+  apiKey: process.env.REACT_APP_API,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  projectId: process.env.REACT_APP_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: "1041323531544",
+  appId: "1:1041323531544:web:9770807461cafbe029126d",
 };
 
 // Initialize Firebase
