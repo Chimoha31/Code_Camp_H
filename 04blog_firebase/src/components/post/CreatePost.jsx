3e96ac2c9b38ac39f858ab35810d91ac83574110
Chimoha31 <mokochii1108@gmail.com>
2022-12-20 20:57:13 -0800

    Styling post

--- 04blog_firebase/src/components/post/CreatePost.jsx
+++ 04blog_firebase/src/components/post/CreatePost.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./CreatePost.css";
+
 
 const CreatePost = () => {
-  return (
-    <div>
-      Post
-    </div>
-  )
-}
+  const [title, setTitle] = useState("");
+  const [postText, setPostText] = useState("");
 
-export default CreatePost
+  const handlePost = () => {
+    console.log(title);
+    console.log(postText);
+  };
+
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>Post Article</h1>
+        <div className="inputPost">
+          <div>Title</div>
+          <input
+            type="text"
+            placeholder="Write title..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="inputPost">
+          <div>Post</div>
+          <textarea
+            placeholder="Write a blog here..."
+            value={postText}
+            onChange={(e) => setPostText(e.target.value)}
+          ></textarea>
+        </div>
+        <button className="postButton" onClick={handlePost}>
+          POST
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
