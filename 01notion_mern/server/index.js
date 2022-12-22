8d593b7e5516a447c60769bd3c541331d4e41170
Chimoha31 <mokochii1108@gmail.com>
2022-12-21 22:48:30 -0800

    Set minimum client &  server

--- 01notion_mern/server/index.js
+++ 01notion_mern/server/index.js
@@ -1,1 +1,13 @@
-console.log("Test");
+const express = require("express");
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("App")
+})
+
+app.listen(PORT, () => {
+  console.log("Server is running 🚀");
+});
+
