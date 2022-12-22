1f7fbc7b61f1dccbe73f1183ff91eacd6c8493f3
Chimoha31 <mokochii1108@gmail.com>
2022-12-22 08:59:11 -0800

    Installed crypto-js

--- 01notion_mern/server/index.js
+++ 01notion_mern/server/index.js
@@ -1,22 +1,30 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
-require('dotenv').config()
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
-const PORT = 5000;
-
-app.get("/", (req, res) => {
-  res.send("App")
-})
+const PORT = 8000;
 
 // Connect mongoDB
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log("Succesfully connected to mongoDB 🎉")
-}).catch((err) => {
-  console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Succesfully connected to mongoDB 🎉");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// API
+app.post("/resister", (req, res) => {
+  try {
+    const password = req.body.password;
+    
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Server is running 🚀");
 });
-
