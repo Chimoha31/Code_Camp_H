62f3119f1eb71e7e104fd2171c1ff9662ee570ae
Chimoha31 <mokochii1108@gmail.com>
2022-12-27 19:26:37 -0800

    AuthLayout

--- 01notion_mern/client/src/components/layout/AuthLayout.jsx
+++ 01notion_mern/client/src/components/layout/AuthLayout.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { Container, Box } from "@mui/system";
+import notion from "../../assets/images/notion.png";
 
 // Resister & Login 共通design
 const AuthLayout = () => {
   return (
     <div>
-      AuthLayout
-      <Outlet />
+      <Container component="main" maxWidth="xs">
+        <Box
+          sx={{
+            marginTop: 6,
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img
+            src={notion}
+            alt=""
+            style={{ width: 100, height: 100, marginBottom: 3 }}
+          />
+          Notion Clone Development
+        </Box>
+        <Outlet />
+      </Container>
     </div>
   );
 };
--- 01notion_mern/client/src/pages/Resister.jsx
+++ 01notion_mern/client/src/pages/Resister.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Box, TextField } from "@mui/material";
 
 const Resister = () => {
   return (
-    <div>
-      Resister
-    </div>
-  )
-}
+    <Box component="form">
+      <TextField
+        fullWidth
+        id="username"
+        label="username"
+        margin="normal"
+        name="username"
+        required
+      />
+      <TextField
+        fullWidth
+        id="password"
+        label="password"
+        margin="normal"
+        name="password"
+        type="password"
+        required
+      />
+      <TextField
+        fullWidth
+        id="confirmPassword"
+        label="Confirm Password"
+        margin="normal"
+        name="confirmPassword"
+        type="password"
+        required
+      />
+    </Box>
+  );
+};
 
-export default Resister
+export default Resister;
