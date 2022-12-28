76e81e38e4b7dfb855473f1cccb3fea4897855bc
Chimoha31 <mokochii1108@gmail.com>
2022-12-27 20:58:39 -0800

    Installed axios

--- 01notion_mern/client/src/pages/Resister.jsx
+++ 01notion_mern/client/src/pages/Resister.jsx
@@ -1,36 +1,53 @@
 import React from "react";
-import { Box, TextField } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Box, Button, TextField } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 
 const Resister = () => {
   return (
-    <Box component="form">
-      <TextField
-        fullWidth
-        id="username"
-        label="username"
-        margin="normal"
-        name="username"
-        required
-      />
-      <TextField
-        fullWidth
-        id="password"
-        label="password"
-        margin="normal"
-        name="password"
-        type="password"
-        required
-      />
-      <TextField
-        fullWidth
-        id="confirmPassword"
-        label="Confirm Password"
-        margin="normal"
-        name="confirmPassword"
-        type="password"
-        required
-      />
-    </Box>
+    <>
+      <Box component="form">
+        <TextField
+          fullWidth
+          id="username"
+          label="username"
+          margin="normal"
+          name="username"
+          required
+        />
+        <TextField
+          fullWidth
+          id="password"
+          label="password"
+          margin="normal"
+          name="password"
+          type="password"
+          required
+        />
+        <TextField
+          fullWidth
+          id="confirmPassword"
+          label="Confirm Password"
+          margin="normal"
+          name="confirmPassword"
+          type="password"
+          required
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          fullWidth
+          type="submit"
+          loading={false}
+          color="primary"
+          variant="outlined"
+        >
+          Create Account
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/login">
+        Already have an account? Go to Login.
+      </Button>
+    </>
   );
 };
 
